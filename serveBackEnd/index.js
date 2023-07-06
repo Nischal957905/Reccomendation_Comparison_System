3855e5679017d882b18939a86ca02749545f8c68
Nischal957905 <nency.gole@gmail.com>
2023-07-06 17:52:12 +0545

    Backend MongoDb Server Setup Done

--- serveBackEnd/index.js
+++ serveBackEnd/index.js
@@ -1,15 +1,62 @@
-import express from "express";
-import cors from "cors";
+import cors from "cors"
+import dotenv from "dotenv"
+import express from "express"
+import mongoose from "mongoose"
+import { fileURLToPath } from 'url'
+import { dirname,join } from 'path'
+import root from './routes/main.js'
+import cookieParser from "cookie-parser"
+import error from "./middleware/error.js"
+import dbConnection from './config/dbConnection.js'
+import { logger, eventLogs } from './middleware/logger.js'
+import core from './config/continuousOperatingReference.js'
+
+
+dotenv.config();
 
 const app = express()
-app.use(cors())
-app.use(express.json());
+app.use(logger)
+app.use(cors(core))
+app.use(express.static('public'))
+app.use('/',root);
+app.use(express.json())
+app.use(cookieParser())
+dbConnection()
+
+const PORT = process.env.port || 8800
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.get("/", (req,res)=>{
     res.json("Response Given")
 })
 
-app.listen(8800, ()=>{
-    console.log("Connected to Server")
+app.all('*',(req,res) => {
+    res.status(404)
+    if(req.accepts('html')){
+        const viewPath = join(__dirname, '.', 'view', 'Error.html');
+        res.sendFile(viewPath)
+    }
+    else if(req.accepts('json')){
+        res.json({message: '404 Not found'})
+    }
+    else{
+        res.type('txt').send('404 not found')
+    }
 })
+
+app.use(error)
+
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDb.")
+    app.listen(PORT, ()=>{
+        console.log("Connected to Server")
+    })
+})
+
+mongoose.connection.on('error', errors => {
+    console.log(errors)
+    eventLogs(`${errors.no}: ${errors.code}\t${errors.syscall}\t${errors.hostname}`, 'mongoErrorLogs.log')
+})
+
 
---
+++ serveBackEnd/middleware/logger.js
@@ -0,0 +1,32 @@
+import { format } from 'date-fns';
+import { v4 as uuid } from 'uuid';
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { promises as fsPromises, existsSync, mkdir } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const eventLogs = async (message, logFileName) => {
+  const formatDate = format(new Date(), 'yyyyMMdd\tHH:mm:ss');
+  const dateLogs = `${formatDate}\t${uuid()}\t${message}\n`;
+
+  try {
+    const logsDir = join(__dirname, '..', 'logs');
+    if (!existsSync(logsDir)) {
+      await mkdir(logsDir);
+    }
+    await fsPromises.appendFile(join(logsDir, logFileName), dateLogs);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const logger = (req, res, next) => {
+  eventLogs(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log');
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+export { logger, eventLogs };
